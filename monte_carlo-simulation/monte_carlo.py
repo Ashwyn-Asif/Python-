2b6310e30f511cb8da379e79fabf70089bead072
Ashwyn Asif <ashwynasif556@gmail.com>
2026-04-22 16:39:20 +0200

    Add imports and first function

---
+++ monte_carlo-simulation/monte_carlo.py
@@ -0,0 +1,24 @@
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+
+TICKER = "AAPL"
+START_PRICE = 260.0
+DRIFT = 0.0003
+VOLATILITY = 0.013
+DAYS = 30
+SIMULATIONS = 1000
+CONFIDENCE = 0.95
+INVESTMENT = 10000.0
+
+def simulate_price_paths(start_price,drift,volatility,days,simulations):
+    price_paths = np.zeros((days,simulations))
+    price_paths[0] = start_price
+    
+    for day in range (1,days):
+        random_shocks = np.random.normal(0,1,simulations)
+        price_paths[day] = price_paths[day-1]*(1+drift+volatility*random_shocks)
+    
+    return price_paths
+
+
